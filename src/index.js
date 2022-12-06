35367143bd68f08935741c160e370f7076ed5ce7
skillzl <user@skillzl.me>
2022-12-06 20:06:22 +0200

    📌 chore: ctx api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 require("dotenv").config();
 require("./utils/Types")();
+import { api } from "@nitric/sdk";
+
+const helloApi = api('main');
+
+helloApi.get("/hello/:name", async (ctx) => {
+    const { name } = ctx.req.params;
+
+    ctx.res.body = `Hello ${name}`;
+
+    return ctx;
+});
 
 const Bot = require("./struct/Bot");
 const client = new Bot();
