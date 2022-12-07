bcf2dd9181a0e7b213aef3d2d61a9770eb64018f
skillzl <user@skillzl.me>
2022-12-07 18:17:21 +0200

    📂 src: struct index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 require("dotenv").config();
 require("./utils/Types")();
-import { api } from "@nitric/sdk";
-
-const helloApi = api('main');
-
-helloApi.get("/hello/:name", async (ctx) => {
-    const { name } = ctx.req.params;
-
-    ctx.res.body = `Hello ${name}`;
-
-    return ctx;
-});
 
 const Bot = require("./struct/Bot");
 const client = new Bot();
